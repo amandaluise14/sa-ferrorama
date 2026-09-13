23635106c310ecebabb292709a7859b7ba56e08d
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-09-13 15:52:22 -0300

    add - adicionei o arquivo para a página de login e adicionei a primeira parte do script para o banco de dados.

---
+++ database/ferrorama.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE sa_ferrorama
+USE sa_ferrorama
+
+CREATE TABLE usuarios (
+id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+email VARCHAR(100) NOT NULL,
+senha VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE trens (
+id_trem INT PRIMARY KEY AUTO_INCREMENT NOT NOT,
+modelo VARCHAR(50) NOT NULL,
+velocidade VARCHAR(50) NOT NULL,
+linha INT NOT NULL,
+carga VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE sensores (
+
+);
+
+CREATE TABLE rotas (
+id_rota INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nome VARCHAR(50) NOT NULL, 
+estacao_origem VARCHAR(50) NOT NULL, 
+estacao_destino VARCHAR(50) NOT NULL,
+distancia_total VARCHAR(50) NOT NULL,
+sensor_id INT,
+    FOREIGN KEY (sensor_id) REFERENCES sensor(id_sensor)
+);
+
