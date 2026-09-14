116425449c2e500c5fbedee0dbcf24f1c5a8ed25
carine_dos-santos@estudante.sesisenai.org.br <carine_dos-santos@estudante.sesisenai.i>
2026-09-13 23:08:50 -0300

    fix- concertei uma chave q estava fechando no lugar errado

--- public/usuario/editar_usuario.php
+++ public/usuario/editar_usuario.php
@@ -3,11 +3,10 @@
 include '../../infra/conexao.php';
 
 if (isset($_POST['id'])) {
-    $senha = $_POST['id'];
+    $id = $_POST['id'];
     $nome = $_POST['nome'];
     $email = $_POST['email'];
 
-}
 
 $sql = "UPDATE usuarios SET nome = ?, email = ? WHERE id = ?";
 $stmt = $conexao->prepare($sql); 
@@ -19,3 +18,5 @@ if ($stmt->execute() === TRUE) {
     echo "Erro ao atualizar usuário: " . $stmt->error;
 }
 
+} else { echo "ID do usuário não fornecido."; 
+}
