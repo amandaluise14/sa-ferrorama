737f15e014d598dfba9f4fb0cd9ced585c2bcc1c
carine_dos-santos@estudante.sesisenai.org.br <carine_dos-santos@estudante.sesisenai.i>
2026-09-13 22:54:50 -0300

    add- adicionei mensagem de atualização

--- public/usuario/editar_usuario.php
+++ public/usuario/editar_usuario.php
@@ -10,3 +10,12 @@ if (isset($_POST['id'])) {
 }
 
 $sql = "UPDATE usuarios SET nome = ?, email = ? WHERE id = ?";
+$stmt = $conexao->prepare($sql); 
+$stmt->bind_param("ssi", $nome, $email, $id);
+
+if ($stmt->execute() === TRUE) {
+    echo "Usuário atualizado com sucesso!";
+} else {
+    echo "Erro ao atualizar usuário: " . $stmt->error;
+}
+
