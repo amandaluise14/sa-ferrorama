537e272e6dc84661a7974ef586a47d4c314a496d
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-09-13 21:59:00 -0300

    add - reformulei a página de cadastrar sensor

--- public/sensor/adicionar_sensor.php
+++ public/sensor/adicionar_sensor.php
@@ -20,6 +20,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Sensor</title>
+</head>
+<body>
+    <title>Cadastrar Sensor</title>
+    <form method="POST">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" value="<?php echo $sensor['nome']; ?>" required>
+        <br><br>
+        <label for="localizacao">Localizacao:</label>
+        <input type="text" id="localizacao" name="localizacao" value="<?php echo $sensor['localizacao']; ?>" required>
+        <br><br>
+        <label for="descricao_localizacao">Descricao da localizacao:</label>
+        <input type="text" id="descricao_localizacao" name="descricao_localizacao" value="<?php echo $sensor['descricao_localizacao']; ?>">
+        <br><br>
+        <label for="dado_monitorado">Tipo do dado monitorado:</label>
+        <input type="text" id="dado_monitorado" name="dado_monitorado" value="<?php echo $sensor['dado_monitorado']; ?>">
+        <br><br>
+        <button type="submit">Cadastrar Sensor</button>
+    </form> 
+</body>
+</html>
 
 
 
