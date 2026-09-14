60099820e96d41cc814603c466374ad70b080434
anajungg <ana_l_jung@estudante.sesisenai.org.br>
2026-09-13 22:07:53 -0300

    add- adicionei a página de excluir rota e atualizei uma escrita errada nas outras páginas já realizadas de exclusão

--- public/sensor/excluir_sensor.php
+++ public/sensor/excluir_sensor.php
@@ -8,7 +8,7 @@ $stmt = $conexao-> prepare ($sql);
 $stmt-> bind_param ("i", $id);
 
 if ($stmt->execute ()) {
-    echo "O sensor fou excluído com sucesso! <br>";
+    echo "O sensor foi excluído com sucesso! <br>";
     echo "<button type='button' onclick=\"window.location.href='../../index.php'\">Voltar</button>";
 }  else {
     echo "Erro ao excluir o sensor: ". $conexao->error;
