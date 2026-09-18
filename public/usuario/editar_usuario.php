4f56d08ac54e21c3bbcc992edef375d01f31080d
gabb <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-09-18 17:37:24 -0300

    up- fiz algumas alterações no código, para melhor entendimento

--- public/usuario/editar_usuario.php
+++ public/usuario/editar_usuario.php
@@ -17,8 +17,7 @@ if ($stmt->execute() === TRUE) {
 } else {
     echo "Erro ao atualizar usuário: " . $stmt->error;
 }
-
-} else { echo "ID do usuário não fornecido."; }
+ }
 
 ?>
  
@@ -28,25 +27,48 @@ if ($stmt->execute() === TRUE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar administrador/usuário</title>
+     <link rel="stylesheet" href="../../style/style.css">
 </head>
 <body>
-   
-      
+    <div class="container">
+    <div class="sidebar">
 
-    <h2>Editar administrador/usuário</h2>
-    <form method="POST">
+    <div class="logo">
+        <img id="imagem_logo" src="../../assets/image/logo_png_branca.png" alt="Logo Info Trem">
+        </div>
+
+    <ul>
+        <li>Início</li>
+        <li class="active"> Sensores e Trens</li>
+        <li>Monitoramento</li>
+        <li>Relatórios</li>
+        <li>Adicionar usuário</li>
+        <li>Sair</li>
+    </ul>
+    </div>
+     <div class= "formulario-container">
+        <form method="POST" class="forms_sensores">
+            <h2 id="titulo-admin"Editar usuario</h2>
+            <div class="conteudo">
+
+        <h2>Editar administrador/usuário</h2>
+    
         <label for="nome">Nome completo</label>
         <input type="text" id="nome" name="nome" required>
         <br><br>
+
         <label for="email">E-mail</label>
         <input type="email" id="email" name="email" required>
         <br><br>
+
         <label for="telefone">Senha</label>
         <input type="text" id="telefone" name="telefone">
         <br><br>
+
         <label for="endereco">Confirmar senha</label>
         <input type="text" id="endereco" name="endereco">
         <br><br>
+        
         <label for="endereco">Status</label>
         <input type="text" id="endereco" name="endereco">
         <br><br>
