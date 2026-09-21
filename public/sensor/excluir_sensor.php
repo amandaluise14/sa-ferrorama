52b0611891fab716acef2167384065024488db59
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-09-21 08:36:19 -0300

    fix - corrigi erros no código da página de listar e excluir sensores.

--- public/sensor/excluir_sensor.php
+++ public/sensor/excluir_sensor.php
@@ -3,15 +3,18 @@
 $id = $_GET ['id'];
 include '../../infra/conexao.php';
 
-$sql = "DELETE FROM sensores WHERE id = ?";
+$sql = "DELETE FROM sensores WHERE id_sensor = ?";
 $stmt = $conn-> prepare ($sql);
 $stmt-> bind_param ("i", $id);
 
 if ($stmt->execute ()) {
     echo "O sensor foi excluído com sucesso! <br>";
-    echo "<button type='button' onclick=\"window.location.href='../../index.php'\">Voltar</button>";
+    echo "<button type='button' onclick=\"window.location.href='../../listar_sensor.php'\">Voltar</button>";
 }  else {
     echo "Erro ao excluir o sensor: ". $conn->error;
 }
+
+$stmt->close();
+$conn->close();
 
 ?>
