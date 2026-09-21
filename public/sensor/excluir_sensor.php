e0c40061601f188912dca6127565d5a3235435b1
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-09-21 09:07:34 -0300

    style - arrumei o style da página de listar sensor.

--- public/sensor/excluir_sensor.php
+++ public/sensor/excluir_sensor.php
@@ -1,6 +1,6 @@
 <?php
 
-$id = $_GET ['id'];
+$id = $_GET ['id_sensor'];
 include '../../infra/conexao.php';
 
 $sql = "DELETE FROM sensores WHERE id_sensor = ?";
@@ -9,7 +9,7 @@ $stmt-> bind_param ("i", $id);
 
 if ($stmt->execute ()) {
     echo "O sensor foi excluído com sucesso! <br>";
-    echo "<button type='button' onclick=\"window.location.href='../../listar_sensor.php'\">Voltar</button>";
+    echo "<button type='button' onclick=\"window.location.href='listar_sensor.php'\">Voltar</button>";
 }  else {
     echo "Erro ao excluir o sensor: ". $conn->error;
 }
